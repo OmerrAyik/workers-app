814f1ecff01ede4a8e5ccad09d7055d3811dfc7c
Omer ayik <omer.ayik@avsos.ai>
2025-02-14 17:43:17 +0300

    errormodal react portale dönüştürüldü

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
-import Button from './Button'
-import Card from './Card'
+import Button from "./Button";
+import Card from "./Card";
+import React, {Fragment} from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return (
+    <div
+      className="backdrop-blur-sm bg-white/30 fixed w-screen h-screen top-0 left-0"
+      onClick={props.onConfirm}
+    ></div>
+  );
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className="error-modal">
+      <Card className="w-[36rem] !p-0 z-20">
+        <header className="bg-red-700 p-4  rounded-t-xl">
+          <h2 className="text-center text-xl text-white">{props.title}</h2>
+        </header>
+        <section className="p-4">{props.message}</section>
+        <footer className="p-4 flex justify-end">
+          <Button className="w-32" onClick={props.onConfirm}>
+            Tamam
+          </Button>
+        </footer>
+      </Card>
+    </div>
+  );
+};
 
 const ErrorModal = (props) => {
-    const {onConfirm,error}=props;
-    const {title,message}=error
-    return (
-        <div className='error-modal'>
-            <div className='backdrop-blur-sm bg-white/30 top-0 fixed w-screen h-screen' onClick={onConfirm}></div>
-            <Card className="w-[36rem] z-20">
-                <header className='bg-red-600 ml-4 mr-4 mt-4 p-3 rounded-lg text-xl relative bottom-4 text-white'>{title}</header>
-                <section className='ml-4 mr-4 mt-4 p-3 rounded-lg text-xl relative bottom-4'>{message}</section>
-                <footer className='flex justify-end mr-6'>
-                    <Button onClick={onConfirm} className="w-55">Tamam</Button>
-                </footer>
-            </Card>
+  const { onConfirm, error } = props;
+  const { title, message } = error;
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById("Blur-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
+        document.getElementById("ModalOverlay")
+      )}
+    </Fragment>
+  );
+};
 
-        </div>
-    )
-}
-
-export default ErrorModal
+export default ErrorModal;
